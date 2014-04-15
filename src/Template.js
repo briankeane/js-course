71edcd850ebd256fd8e4557b1cfa194044e06a0e
Brian Keane <lonesomewhistle@gmail.com>
2014-04-15 10:17:17 -0500

    done

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,17 @@
-var render = function(){
-
+var render = function(template, obj){
+  var str = template.replace(/\{\{(.*?)\}\}/g,  function(g, match) {
+    return obj[match];
+  });
+  return str;
 };
+
+// var render = function(template, obj){
+//   var str = template;
+//   while (str.match(/\{\{(.*?)\}\}/)) {
+//     debugger;
+//     str.replace(/\{\{(.*?)\}\}/, function(i, match) {
+//       return obj[match];
+//     });
+//     return str;
+//   }
+// };
